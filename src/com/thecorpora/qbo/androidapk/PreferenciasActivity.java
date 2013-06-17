178545e6c869201a8d099bd4ae715b149912689a
Pablo <pablo@cosificando.com>
2013-06-17 19:25:37 +0200

    Nueva Activity sobre preferencias

---
+++ src/com/thecorpora/qbo/androidapk/PreferenciasActivity.java
@@ -0,0 +1,17 @@
+package com.thecorpora.qbo.androidapk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.robotsandtweets.r2builders.*;
+/**
+ * Created by pablo_001 on 13/06/13.
+ */
+public class PreferenciasActivity extends Activity {
+
+    private Preferences preferences;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.preferences = savedInstanceState.
+    }
+}
